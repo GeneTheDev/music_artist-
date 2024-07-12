7437d9d7ee6aad2940fdbc9d7af567dea4d7ab0b
Eugene Owens <eugeneowens691@gmail.com>
2024-07-11 22:58:54 -0400

    removing bootstrap adding tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
-import { useEffect } from "react";
 import "../styles/globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../components/comm/Navbar";
 
 function App({ Component, pageProps }) {
-  useEffect(() => {
-    // Ensure Bootstrap JavaScript is included
-    import("bootstrap/dist/js/bootstrap.bundle.min");
-  }, []);
 
   return (
     <>

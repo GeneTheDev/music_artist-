a29b345b9c33096032d4b9a9ed9c486ab1b37c46
gene21gene <owens.eugene@yahoo.com>
2024-07-18 04:01:49 -0400

    adding Link

--- components/comm/Navbar.jsx
+++ components/comm/Navbar.jsx
@@ -7,6 +7,7 @@ import youtubeIcon from '@iconify/icons-simple-icons/youtube';
 import instagramIcon from '@iconify/icons-simple-icons/instagram';
 import facebookIcon from '@iconify/icons-simple-icons/facebook';
 import twitterIcon from '@iconify/icons-simple-icons/twitter';
+import Link from "next/link";
 
 const Navbar = ({ site }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,9 +18,9 @@ const Navbar = ({ site }) => {
     }}>
       <div className="container mx-auto flex flex-wrap items-center justify-between px-4 py-2">
         <div className="logo flex justify-between w-full md:w-auto">
-          <a href="/" className="block w-24 h-auto rounded-lg p-2 cursor-pointer">
+          <Link href="/" className="block w-24 h-auto rounded-lg p-2 cursor-pointer">
             EliEzer
-          </a>
+          </Link>
           <button onClick={() => setIsOpen(!isOpen)} className="md:hidden block">
             <GiHamburgerMenu size={30} className="text-white" />
           </button>
@@ -27,45 +28,45 @@ const Navbar = ({ site }) => {
         <div className={`md:flex items-center ${isOpen ? 'block' : 'hidden'} w-full md:w-auto`}>
           <ul className="flex flex-col md:flex-row items-center md:space-x-8 w-full md:w-auto">
             <li className="list-none my-2 md:my-0 mx-5 text-lg">
-              <a href="/" className="hover:text-gray-300 block">Home</a>
+              <Link href="/" className="hover:text-gray-300 block">Home</Link>
             </li>
             <li className="list-none my-2 md:my-0 mx-5 text-lg">
-              <a href="/about" className="hover:text-gray-300 block">About</a>
+              <Link href="/about" className="hover:text-gray-300 block">About</Link>
             </li>
             <li className="list-none my-2 md:my-0 mx-5 text-lg">
-              <a href="/services" className="hover:text-gray-300 block">Stream</a>
+              <Link href="/services" className="hover:text-gray-300 block">Stream</Link>
             </li>
           </ul>
           <ul className="flex flex-wrap items-center justify-center space-x-4 w-full md:w-auto mt-4 md:mt-0 md:ml-8">
             <li className="list-none inline-block mx-2 md:mx-0">
-              <a href={site.platforms.spotify} target="_blank" rel="noopener noreferrer">
+              <Link href={site.platforms.spotify} target="_blank" rel="noopener noreferrer">
                 <Icon icon={spotifyIcon} className="text-2xl" />
-              </a>
+              </Link>
             </li>
             <li className="list-none inline-block mx-2 md:mx-0">
-              <a href={site.platforms.soundcloud} target="_blank" rel="noopener noreferrer">
+              <Link href={site.platforms.soundcloud} target="_blank" rel="noopener noreferrer">
                 <Icon icon={soundcloudIcon} className="text-2xl" />
-              </a>
+              </Link>
             </li>
             <li className="list-none inline-block mx-2 md:mx-0">
-              <a href={site.platforms.youtube} target="_blank" rel="noopener noreferrer">
+              <Link href={site.platforms.youtube} target="_blank" rel="noopener noreferrer">
                 <Icon icon={youtubeIcon} className="text-2xl" />
-              </a>
+              </Link>
             </li>
             <li className="list-none inline-block mx-2 md:mx-0">
-              <a href={site.socials.instagram} target="_blank" rel="noopener noreferrer">
+              <Link href={site.socials.instagram} target="_blank" rel="noopener noreferrer">
                 <Icon icon={instagramIcon} className="text-2xl" />
-              </a>
+              </Link>
             </li>
             <li className="list-none inline-block mx-2 md:mx-0">
-              <a href={site.socials.facebook} target="_blank" rel="noopener noreferrer">
+              <Link href={site.socials.facebook} target="_blank" rel="noopener noreferrer">
                 <Icon icon={facebookIcon} className="text-2xl" />
-              </a>
+              </Link>
             </li>
             <li className="list-none inline-block mx-2 md:mx-0">
-              <a href={site.socials.twitter} target="_blank" rel="noopener noreferrer">
+              <Link href={site.socials.twitter} target="_blank" rel="noopener noreferrer">
                 <Icon icon={twitterIcon} className="text-2xl" />
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
